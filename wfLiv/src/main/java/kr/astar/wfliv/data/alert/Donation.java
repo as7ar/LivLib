79fd50a84895ff83c2215b90525d1745e54ebe79
ASTAR <santabes93@gmail.com>
2026-04-03 15:20:16 +0900

    Convert Donation record to regular class

--- wfLiv/src/main/java/kr/astar/wfliv/data/alert/Donation.java
+++ wfLiv/src/main/java/kr/astar/wfliv/data/alert/Donation.java
@@ -1,13 +1,49 @@
 package kr.astar.wfliv.data.alert;
 
-public record Donation(
-        User user,
-        String content,
-        long amount,
-        boolean isTest,
-        long timestamp,
-        DonationData donationData,
-        PlatformData platformData
-) {
-}
+public class Donation {
+    private final User user;
+    private final String content;
+    private final long amount;
+    private final boolean isTest;
+    private final long timestamp;
+    private final DonationData donationData;
+    private final PlatformData platformData;
 
+    public Donation(User user, String content, long amount, boolean isTest, long timestamp, DonationData donationData, PlatformData platformData) {
+        this.user = user;
+        this.content = content;
+        this.amount = amount;
+        this.isTest = isTest;
+        this.timestamp = timestamp;
+        this.donationData = donationData;
+        this.platformData = platformData;
+    }
+
+    public User user() {
+        return user;
+    }
+
+    public String content() {
+        return content;
+    }
+
+    public long amount() {
+        return amount;
+    }
+
+    public boolean isTest() {
+        return isTest;
+    }
+
+    public long timestamp() {
+        return timestamp;
+    }
+
+    public DonationData donationData() {
+        return donationData;
+    }
+
+    public PlatformData platformData() {
+        return platformData;
+    }
+}
