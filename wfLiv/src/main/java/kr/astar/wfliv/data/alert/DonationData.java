4b5b27e81eb54d04d38e4e32093c6dfed02c56bc
ASTAR <santabes93@gmail.com>
2026-04-03 15:21:14 +0900

    Convert DonationData record to regular class

--- wfLiv/src/main/java/kr/astar/wfliv/data/alert/DonationData.java
+++ wfLiv/src/main/java/kr/astar/wfliv/data/alert/DonationData.java
@@ -1,8 +1,31 @@
 package kr.astar.wfliv.data.alert;
 
-public record DonationData(
-        String page,
-        String idx,
-        String pageid,
-        String preset
-) {}
+public class DonationData {
+    private final String page;
+    private final String idx;
+    private final String pageid;
+    private final String preset;
+
+    public DonationData(String page, String idx, String pageid, String preset) {
+        this.page = page;
+        this.idx = idx;
+        this.pageid = pageid;
+        this.preset = preset;
+    }
+
+    public String page() {
+        return page;
+    }
+
+    public String idx() {
+        return idx;
+    }
+
+    public String pageid() {
+        return pageid;
+    }
+
+    public String preset() {
+        return preset;
+    }
+}
